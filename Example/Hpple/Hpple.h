03096c891effcc14e7a44d1ce81069f1c56ef3fe
hfy <hellohfy@gmail.com>
2016-10-27 17:59:57 +0800

    添加framework

---
+++ Example/Hpple/Hpple.h
@@ -0,0 +1,21 @@
+//
+//  Hpple.h
+//  Hpple
+//
+//  Created by HeFengyang on 2016/10/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hpple.
+FOUNDATION_EXPORT double HppleVersionNumber;
+
+//! Project version string for Hpple.
+FOUNDATION_EXPORT const unsigned char HppleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hpple/PublicHeader.h>
+//#import <Hpple/TFHpple.h>
+
+#import "TFHpple.h"
+#import "XPathQuery.h"
